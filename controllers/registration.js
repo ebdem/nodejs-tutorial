81228bce8ad54b4f657799c476efa8f18b710c08
Ramazan Cetin <ramazancetinn@yandex.com>
2020-05-15 22:34:04 +0300

    registration endpoint added

--- controllers/registration.js
+++ controllers/registration.js
@@ -6,3 +6,70 @@
 
     // eğer yeni bri kayıt ise username ve password database.json'a kaydeilecek
     // bu işlemler sırasında database.json dosyasındaki veriler ASLA KAYBOLMASIN
+
+const fs = require('fs');
+const path = require('path');
+
+    function registration(req, res) {
+
+        const {
+            username,
+            password, 
+            name, 
+            lname, 
+            email, 
+            hoby, 
+            sex
+        } = req.body;
+
+        // const username = req.body.username;
+        // const password = req.body.password;
+        // const name = req.body.name;
+        // const lname = req.body.lname;
+        // const email = req.body.email;
+        // const hoby = req.body.hoby;
+        // const sex = req.body.sex;
+
+
+        const dbPath = path.join(__dirname, 'database.json')
+
+        const userTable = require('./database.json')
+
+        // check if email exist
+        userTable.map(element =>{
+            if (element.email === email) {
+                return res.json({ok: false, desc:'email already exist!'})
+            }
+        })
+
+
+        userTable.push({
+            username,
+            password,
+            email,
+            name,
+            lname,
+            hoby,
+            sex
+        })
+
+        // userTable.append({
+        //     username: username,
+        //     password: password,
+        //     email: email,
+        //     name: name,
+        //     lname: lname,
+        //     hoby: hoby,
+        //     sex: sex
+        // })
+
+
+        const writeData = JSON.stringify(userTable);
+        
+        fs.writeFileSync(dbPath, writeData);
+
+        return res.json({ok: true, desc:'User successfully registrated'})
+
+    }
+
+    module.exports = registration
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
-app.use(express.json()); //use express json for rest api json requests
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()); //use express json for rest api json requests
 app.use(cors())
 const PORT = 4444; //assign PORT to 4444
 console.log("ebubekir");
@@ -10,9 +13,11 @@ console.log("ebubekir");
 // controller imports
 const homeController = require('./controllers/home');
 const loginController = require('./controllers/login');
+const registrationController = require('./controllers/registration');
 
 app.get('/', homeController);
 app.post('/login', loginController);
+app.post('/registration', registrationController);
 
 
 // start server
