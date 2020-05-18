7e6e4e9e2e6b867b135402b6be6ea877e12f650b
Ramazan Cetin <ramazancetinn@yandex.com>
2020-05-18 23:49:28 +0300

    Add mysql connector and rewrite registration

--- controllers/registration.js
+++ controllers/registration.js
@@ -1,75 +1,54 @@
 // registration function
 
-    // gönderilen username ve password önce databse.json'dan kontrol edilecek
-    // eğer username databse.json dosyadında önceden varsa
-    // ressponse olrak user zaten var dönülecek
+// gönderilen username ve password önce databse.json'dan kontrol edilecek
+// eğer username databse.json dosyadında önceden varsa
+// ressponse olrak user zaten var dönülecek
 
-    // eğer yeni bri kayıt ise username ve password database.json'a kaydeilecek
-    // bu işlemler sırasında database.json dosyasındaki veriler ASLA KAYBOLMASIN
+// eğer yeni bri kayıt ise username ve password database.json'a kaydeilecek
+// bu işlemler sırasında database.json dosyasındaki veriler ASLA KAYBOLMASIN
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '1234',
+    database: 'nodejs_tutorial'
+})
 
-const fs = require('fs');
-const path = require('path');
+async function registration(req, res) {
 
-    function registration(req, res) {
+    const username = req.body.username 
+    const password = req.body.password 
+    const email = req.body.email 
+    const name = req.body.name 
+    const surname = req.body.surname 
+    const sex = req.body.sex 
+    const age = req.body.age;
 
-        const {
-            username,
-            password, 
-            name, 
-            lname, 
-            email, 
-            hoby, 
-            sex
-        } = req.body;
-
-        // const username = req.body.username;
-        // const password = req.body.password;
-        // const name = req.body.name;
-        // const lname = req.body.lname;
-        // const email = req.body.email;
-        // const hoby = req.body.hoby;
-        // const sex = req.body.sex;
-
-
-        const dbPath = path.join(__dirname, 'database.json')
-
-        const userTable = require('./database.json')
-
-        // check if email exist
-        userTable.map(element =>{
-            if (element.email === email) {
-                return res.json({ok: false, desc:'email already exist!'})
-            }
-        })
-
-
-        userTable.push({
-            username,
-            password,
-            email,
-            name,
-            lname,
-            hoby,
-            sex
-        })
-
-        // userTable.append({
-        //     username: username,
-        //     password: password,
-        //     email: email,
-        //     name: name,
-        //     lname: lname,
-        //     hoby: hoby,
-        //     sex: sex
-        // })
-
-
-        const writeData = JSON.stringify(userTable);
-        
-        fs.writeFileSync(dbPath, writeData);
-
-        return res.json({ok: true, desc:'User successfully registrated'})
-
+    if(!username || !email || !name || !surname || !password){
+        return res.json({ok:false, desc: 'username, email, password, name and surname can not be null'})
     }
 
-    module.exports = registration
+    // check or username is already exist
+    // if email  exist return message (This email is already registrated try to login.)
+
+    connection.connect(function (err) {
+        if (err){throw err}
+        console.log("Connected");
+    })
+
+    connection.query(`INSERT INTO Users (Username, Email, Password, Name, Surname, Sex, Age) 
+        VALUES ('${username}', '${email}', '${password}', '${name}', '${surname}', '${sex}', ${age})`,
+        function (err, rows, fields) {
+            if(err){
+                connection.end()
+                return res.json({ok: false, desc: err})
+            }
+        
+            return res.json({ok: true, desc: 'User has been saved'})
+        })
+
+    // return res.json({ ok: true, desc: 'User successfully registrated' })
+
+}
+
+module.exports = registration
